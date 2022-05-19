eb88f787eba973e885adb374cae0d5c4fcad50b0
Akmal Soliev <soliev@gmail.com>
2022-05-19 12:38:10 +0200

    lesson 7 updated

--- lesson7-classes-subclasses/src/main/scala/Main.scala
+++ lesson7-classes-subclasses/src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import com.sun.beans.editors.ShortEditor
+
 object Main extends App{
 //  class Person(name:String, age:Int){
 //    def presentYourself = {
@@ -8,10 +10,19 @@ object Main extends App{
 //  val jack = new Person("Jack", 2)
 //  jack.presentYourself
 
-val randomFactoryNumber = scala.util.Random.between(0, 100)
-
+  val randomFactoryNumber = scala.util.Random.between(0, 100)
   class Factory(val factoryName:String=s"Factory Number $randomFactoryNumber"){
-
+    val product = "Water"
+    def makeProduct():Unit = println(s"Making $product")
   }
+
+  class ShoeFactory(override val factoryName:String=s"Factory Number $randomFactoryNumber") extends Factory(factoryName){
+    override val product: String = "Shoes"
+  }
+
+  val Nike = new ShoeFactory("Nike1")
+  println(Nike.factoryName)
+  println(Nike.product)
+  Nike.makeProduct()
 
 }
