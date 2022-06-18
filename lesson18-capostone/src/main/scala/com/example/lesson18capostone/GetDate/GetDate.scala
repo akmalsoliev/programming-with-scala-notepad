628182686b9c4fb8f413bbc6870e3e45138c98da
Akmal Soliev <soliev@gmail.com>
2022-06-19 00:16:54 +0200

    small developments on the datetime construct, still need to work on it

--- lesson18-capostone/src/main/scala/com/example/lesson18capostone/GetDate/GetDate.scala
+++ lesson18-capostone/src/main/scala/com/example/lesson18capostone/GetDate/GetDate.scala
@@ -2,20 +2,38 @@ package com.example.lesson18capostone.GetDate
 
 //import scala.io.StdIn
 import java.time.ZoneId
-import java.{util => ju}
+import scala.collection.mutable
+import scala.jdk.CollectionConverters._
 
 
 object GetDate {
 //    val userInput:String = StdIn.readLine("Input the location: ")
-    val userInput:String = "UTC"
+    val userInput: String = "Beijing"
 
-    private def getZones: ju.Set[String] = ZoneId.getAvailableZoneIds
+    class GetDT(input: String) {
+        // Retrieve available timezones
+        private def getZones: mutable.Set[String] = ZoneId.getAvailableZoneIds.asScala
+        // Matching function to find if there is a match
+        private def doesMatch(zoneId: String, input: String): Boolean = zoneId.contains(input)
+
+        private def findZone(location: String): ZoneId = ZoneId.of(location)
+        // Get the timezone based on the match, not perfect and prone to errors, but will do for now
+        private def getLocation: Option[String] = getZones.find(zoneId => doesMatch(zoneId, input))
+        def getHead: Any = try {
+            val zoneLocation: ZoneId = findZone(getLocation.head)
+            java.time.LocalDateTime.now(zoneLocation)
+        } catch {
+            case _: NoSuchElementException => s"$input is an invalid timezone"
+        }
+
+        // Current datetime
+//        val currentDateTime: java.time.LocalDateTime = java.time.LocalDateTime.now(findZone)
+
+    }
+
+
 
     def main(args:Array[String]): Unit = {
-        getZones.forEach{
-            tz =>
-                if (tz.contains(userInput)) println(tz)
-                else println("Not found ")
-        }
+        println(new GetDT(userInput).getHead)
     }
 }
